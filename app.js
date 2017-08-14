b15a9d06945d124ca2006332ae197ce1ec138c0d
Joilence <cjyk84719@gmail.com>
2017-08-14 17:16:45 +0800

    adding new parts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const session = require('koa-session2');
 const app = new Koa();
 const router = require('koa-router')();
 const views = require('koa-views');
@@ -11,6 +12,7 @@ const logger = require('koa-logger');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
+const Store = require("./store.js");
 
 // middlewares
 app.use(convert(bodyparser));
@@ -40,6 +42,85 @@ app.on('error', function(err, ctx){
   console.log(err)
   logger.error('server error', err, ctx);
 });
+
+
+
+app.use(session({
+  store: new Store()
+}));
+
+
+// socket io
+
+/* Event list
+  - connect
+    - create
+    - join
+      - msg
+      - emo
+    - leave
+  -disconnect
+  - system
+ */
+
+// const io = require('socket.io')();
+//
+// var roomInfo = {};
+//
+// io.on('connection', function (socket) {
+//   console.log('unknown user connected.');
+//
+//   //TODO: auth with cookie or something
+//
+//   socket.on('join', function (info) {
+//     username = info.username;
+//     roomId = info.roomId
+//
+//     // add user into room
+//     if (!roomInfo[roomId]) {
+//       roomInfo[roomId] = [];
+//     } else {
+//       // notify other users in the room
+//       io.to(roomId).emit('system', username + ' join the room.', roomInfo[roomId]);
+//     }
+//     roomInfo[roomId].push(username);
+//     socket.join(roomId);
+//
+//     console.log(username + ' joined ' + roomId + '.');
+//   });
+//
+//   socket.on('leave', function (info) {
+//     var username = info.username;
+//     var roomId = info.roomId;
+//
+//     let index = roomInfo[roomId].indexOf(username);
+//     if (index !== -1) {
+//       roomInfo[roomId].splice(index, 1);
+//     }
+//     socket.leave(roomId);
+//     io.to(roomId).emit('system', username + ' left the room.', roomInfo[roomId]);
+//     console.log(username + ' left ' + roomId + '.');
+//   });
+//
+//   socket.on('disconnect', function () {
+//     //TODO: if the user still in some room?
+//     console.log(username + ' disconnected.');
+//   });
+//
+//   // user send message to the room
+//   socket.on('msg', function (msg) {
+//     // auth
+//     if (roomInfo[roomId].indexOf(user) === -1) {
+//         return false;
+//     }
+//     io.to(roomId).emit('msg', username, msg);
+//   });
+//
+//   socket.on('emo', function (emo) {
+//     //TODO: if user in the room
+//     io.to(roomId).emit('emo', username, emo);
+//   })
+// });
 
 
 module.exports = app;
